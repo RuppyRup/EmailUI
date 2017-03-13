a6b6dccc5bd1afac718d1ccf4564a36b8707d1c9
RuppyRup <rupert.waldron@yahoo.co.uk>
2017-03-13 18:36:46 +0000

    Initial Commit

---
+++ EmailUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmailUI
+//
+//  Created by Rupert Waldron on 13/03/2017.
+//  Copyright © 2017 Rupert Waldron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
